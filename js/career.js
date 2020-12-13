88ca14ee87bfc14d63675b878decea14087de51f
AlaminCoding <alamin.coding@gmail.com>
2020-12-13 20:05:42 +0600

    commit

--- js/career.js
+++ js/career.js
@@ -1,6 +1,8 @@
 const posbtn = document.querySelectorAll(".pos-btn");
 const crossBtn = document.querySelectorAll(".cross-btn");
 const psBox = document.querySelectorAll(".position-box");
+const cLink = document.querySelectorAll(".c-link");
+const circular = document.querySelectorAll(".circular");
 
 posbtn.forEach((element) => {
   element.addEventListener("click", function () {
@@ -10,5 +12,10 @@ posbtn.forEach((element) => {
 crossBtn.forEach((element) => {
   element.addEventListener("click", function () {
     element.parentElement.style.display = "none";
+  });
+});
+cLink.forEach((element) => {
+  element.addEventListener("click", function () {
+    element.nextElementSibling.style.display = "block";
   });
 });
